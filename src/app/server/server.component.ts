9e7e05d9b81e20fc6e57d5ea2755de80d7f3b595
ddeveloper72 <duncan.niamh@gmail.com>
2019-05-07 13:27:51 +0100

    Created a new server.component.ts file

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html' /* relative file path */
+})
+export class ServerComponent {
+
+}
