6f354840a6b8c93a9b792be77068e1e320754539
ddeveloper72 <duncan.niamh@gmail.com>
2019-05-08 13:13:01 +0100

    Added a server.component styleUrls

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html' /* relative file path */
+    templateUrl: './server.component.html', /* relative file path */
+    styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
 
